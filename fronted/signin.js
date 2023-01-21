2630fa395714730954e4818e204d37c9dcbb965a
apsinghrana100 <apsinghrana100@gmail.com>
2023-01-21 21:28:06 +0530

    backend

--- fronted/signin.js
+++ fronted/signin.js
@@ -1,29 +1,64 @@
 const submit=document.getElementById("submit-btn");
+const message=document.getElementById("msg");
 
 submit.addEventListener('click',signup);
 
  async function signup(event)
 {
     try {
+        
         event.preventDefault();
-        const username=document.getElementById('user-name').value;
-        const emailid=document.getElementById("user-email").value;
-        const password=document.getElementById("user-password").value;
+        
+        const username=document.getElementById('user-name').value.trim();
+        const emailid=document.getElementById("user-email").value.trim();
+        const password=document.getElementById("user-password").value.trim();
         console.log(username+" "+emailid+" "+password); 
 
-        const userdetail={
-            username,
-            emailid,
-            password
-        }
-
-        const promise= await axios.post("http://localhost:4000/",userdetail);
-        if(promise)
+        if(username!=="" && username!==undefined && emailid!=="" && emailid!==undefined && password!=="" && password!==undefined)
         {
-            console.log(promise);
+        
+            const userdetail={
+                username,
+                emailid,
+                password
+            }
+
+            const promise= await axios.post("http://localhost:4000/signuppage",userdetail);
+            if(promise.data===true)
+            {
+                console.log("email id exists");
+                message.style.backgroundColor="red";
+                message.innerHTML="email id exits</h1>";
+                setTimeout(() => {
+                    message.style.display = "none";
+                }, 3000);
+                message.style.display = "block";
+
+                    
+                
+            
+            }else{
+                console.log("Data Add Successfully");
+                message.style.backgroundColor="green";
+                message.innerText="Data Add Successfully";
+                setTimeout(() => {
+                    message.style.display="none";
+                }, 3000);
+                message.style.display = "block";
+            }
+
+        }else{
+            console.log("some Field are might be empty");
+            message.style.backgroundColor="red";
+                message.innerText="some Field are might be empty";
+                setTimeout(() => {
+                    message.style.display="none";
+                }, 3000);
+                message.style.display = "block";
         }
-    } catch (error) {
+    }catch (error) {
         console.log(error);
     }
+    
    
 }
